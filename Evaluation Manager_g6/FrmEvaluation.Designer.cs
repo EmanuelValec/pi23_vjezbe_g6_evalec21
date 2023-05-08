afa9108841fa0970b256f23b9b2cfb6602e2f96e
foi <foi@LAB2RM8>
2023-05-08 20:13:12 +0200

    Klasa aktivnost i forma

---
+++ Evaluation Manager_g6/FrmEvaluation.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Evaluation_Manager {
+    partial class FrmEvaluation {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dmn_bodovi = new System.Windows.Forms.DomainUpDown();
+            this.btn_spremi = new System.Windows.Forms.Button();
+            this.btn_odustani = new System.Windows.Forms.Button();
+            this.lbl_aktivnost = new System.Windows.Forms.Label();
+            this.lbl_uvjet_za_potpis = new System.Windows.Forms.Label();
+            this.lbl_uvjet_za_ocjenu = new System.Windows.Forms.Label();
+            this.lbl_ocjenjivac = new System.Windows.Forms.Label();
+            this.lbl_datum_evaluacije = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(25, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(276, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(347, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(201, 280);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(25, 83);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(544, 155);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(469, 45);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(347, 280);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // dmn_bodovi
+            // 
+            this.dmn_bodovi.Location = new System.Drawing.Point(469, 280);
+            this.dmn_bodovi.Name = "dmn_bodovi";
+            this.dmn_bodovi.Size = new System.Drawing.Size(100, 20);
+            this.dmn_bodovi.TabIndex = 6;
+            this.dmn_bodovi.Text = "0";
+            // 
+            // btn_spremi
+            // 
+            this.btn_spremi.Location = new System.Drawing.Point(347, 326);
+            this.btn_spremi.Name = "btn_spremi";
+            this.btn_spremi.Size = new System.Drawing.Size(100, 23);
+            this.btn_spremi.TabIndex = 7;
+            this.btn_spremi.Text = "Spremi";
+            this.btn_spremi.UseVisualStyleBackColor = true;
+            // 
+            // btn_odustani
+            // 
+            this.btn_odustani.Location = new System.Drawing.Point(469, 326);
+            this.btn_odustani.Name = "btn_odustani";
+            this.btn_odustani.Size = new System.Drawing.Size(100, 23);
+            this.btn_odustani.TabIndex = 8;
+            this.btn_odustani.Text = "Odustani";
+            this.btn_odustani.UseVisualStyleBackColor = true;
+            // 
+            // lbl_aktivnost
+            // 
+            this.lbl_aktivnost.AutoSize = true;
+            this.lbl_aktivnost.Location = new System.Drawing.Point(22, 28);
+            this.lbl_aktivnost.Name = "lbl_aktivnost";
+            this.lbl_aktivnost.Size = new System.Drawing.Size(54, 13);
+            this.lbl_aktivnost.TabIndex = 9;
+            this.lbl_aktivnost.Text = "Aktivnost:";
+            // 
+            // lbl_uvjet_za_potpis
+            // 
+            this.lbl_uvjet_za_potpis.AutoSize = true;
+            this.lbl_uvjet_za_potpis.Location = new System.Drawing.Point(344, 28);
+            this.lbl_uvjet_za_potpis.Name = "lbl_uvjet_za_potpis";
+            this.lbl_uvjet_za_potpis.Size = new System.Drawing.Size(80, 13);
+            this.lbl_uvjet_za_potpis.TabIndex = 10;
+            this.lbl_uvjet_za_potpis.Text = "Uvjet za potpis:";
+            this.lbl_uvjet_za_potpis.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_uvjet_za_ocjenu
+            // 
+            this.lbl_uvjet_za_ocjenu.AutoSize = true;
+            this.lbl_uvjet_za_ocjenu.Location = new System.Drawing.Point(466, 28);
+            this.lbl_uvjet_za_ocjenu.Name = "lbl_uvjet_za_ocjenu";
+            this.lbl_uvjet_za_ocjenu.Size = new System.Drawing.Size(84, 13);
+            this.lbl_uvjet_za_ocjenu.TabIndex = 11;
+            this.lbl_uvjet_za_ocjenu.Text = "Uvjet za ocjenu:";
+            // 
+            // lbl_ocjenjivac
+            // 
+            this.lbl_ocjenjivac.AutoSize = true;
+            this.lbl_ocjenjivac.Location = new System.Drawing.Point(198, 264);
+            this.lbl_ocjenjivac.Name = "lbl_ocjenjivac";
+            this.lbl_ocjenjivac.Size = new System.Drawing.Size(57, 13);
+            this.lbl_ocjenjivac.TabIndex = 12;
+            this.lbl_ocjenjivac.Text = "Ocjenjivač";
+            // 
+            // lbl_datum_evaluacije
+            // 
+            this.lbl_datum_evaluacije.AutoSize = true;
+            this.lbl_datum_evaluacije.Location = new System.Drawing.Point(344, 264);
+            this.lbl_datum_evaluacije.Name = "lbl_datum_evaluacije";
+            this.lbl_datum_evaluacije.Size = new System.Drawing.Size(92, 13);
+            this.lbl_datum_evaluacije.TabIndex = 13;
+            this.lbl_datum_evaluacije.Text = "Datum evaluacije:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(466, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Bodovi:";
+            // 
+            // FrmEvaluation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 366);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_datum_evaluacije);
+            this.Controls.Add(this.lbl_ocjenjivac);
+            this.Controls.Add(this.lbl_uvjet_za_ocjenu);
+            this.Controls.Add(this.lbl_uvjet_za_potpis);
+            this.Controls.Add(this.lbl_aktivnost);
+            this.Controls.Add(this.btn_odustani);
+            this.Controls.Add(this.btn_spremi);
+            this.Controls.Add(this.dmn_bodovi);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "FrmEvaluation";
+            this.Text = "FrmEvaluation";
+            this.Load += new System.EventHandler(this.FrmEvaluation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.DomainUpDown dmn_bodovi;
+        private System.Windows.Forms.Button btn_spremi;
+        private System.Windows.Forms.Button btn_odustani;
+        private System.Windows.Forms.Label lbl_aktivnost;
+        private System.Windows.Forms.Label lbl_uvjet_za_potpis;
+        private System.Windows.Forms.Label lbl_uvjet_za_ocjenu;
+        private System.Windows.Forms.Label lbl_ocjenjivac;
+        private System.Windows.Forms.Label lbl_datum_evaluacije;
+        private System.Windows.Forms.Label label1;
+    }
+}
